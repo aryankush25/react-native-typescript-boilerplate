937787dab042a2c202a599a07aca07b5bfa2d0ca
Aryan Agarwal <aryankush025@gmail.com>
2020-11-04 21:53:55 +0530

    React Navigation Installed

--- src/container/AppBoot/App.tsx
+++ src/container/AppBoot/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import 'react-native-gesture-handler';
 import ReduxStore, { persistor } from '../../store';
 import AppContainer from '../AppContainer';
 
