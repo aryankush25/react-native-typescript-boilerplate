377c6537b51f6341125f329363b26ec8c6c5f4c1
Aryan Agarwal <aryankush025@gmail.com>
2020-11-11 03:24:59 +0530

    Change to custom button

--- src/components/shared/CustomButton/index.tsx
+++ src/components/shared/CustomButton/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import {
   Text,
   TouchableOpacity,
@@ -8,61 +8,72 @@ import {
 } from 'react-native';
 
 interface CustomButtonTypes {
+  type?: 'primary' | 'secondary';
   text: string;
   disabled?: boolean;
   loading?: boolean;
   onPress: Function;
 }
 
-const styles = StyleSheet.create({
-  appButtonContainer: {
-    elevation: 4,
-    backgroundColor: 'black',
-    borderRadius: 10,
-    paddingVertical: 10,
-  },
-  appButtonText: {
-    fontSize: 18,
-    color: '#fff',
-    fontWeight: '900',
-  },
-  disabled: {
-    opacity: 0.8,
-  },
-  innerContainer: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    flexDirection: 'row',
-  },
-  activityIndicatorContainer: {
-    marginLeft: 10,
-  },
-});
+const getStyleSheet = (disabled: boolean, type: 'primary' | 'secondary') => {
+  return StyleSheet.create({
+    appButtonContainer: {
+      backgroundColor: disabled
+        ? '#e6e3e6'
+        : type === 'secondary'
+        ? '#ff6786'
+        : 'black',
+      borderRadius: 10,
+      paddingVertical: 14,
+    },
+    innerContainer: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      flexDirection: 'row',
+    },
+    buttonTextContainer: {
+      marginLeft: 10,
+      flex: 1,
+    },
+    buttonText: {
+      fontSize: 18,
+      color: disabled ? '#bcbbbc' : '#fff',
+      fontWeight: '900',
+      width: '100%',
+      textAlign: 'center',
+    },
+    activityIndicatorContainer: {
+      position: 'absolute',
+      right: 20,
+    },
+  });
+};
 
 const CustomButton = ({
+  type = 'primary',
   text,
   disabled = false,
   loading = false,
   onPress,
 }: CustomButtonTypes) => {
-  let touchableOpacityStyles: any = [styles.appButtonContainer];
-
-  if (disabled || loading) {
-    touchableOpacityStyles.push(styles.disabled);
-  }
+  const styles = useMemo(() => {
+    return getStyleSheet(disabled || loading, type);
+  }, [disabled, loading, type]);
 
   return (
     <TouchableOpacity
       onPress={() => (!disabled || !loading) && onPress()}
-      style={touchableOpacityStyles}
+      style={styles.appButtonContainer}
       activeOpacity={0.8}
       disabled={disabled || loading}>
       <View style={styles.innerContainer}>
-        <Text style={styles.appButtonText}>{text}</Text>
+        <View style={styles.buttonTextContainer}>
+          <Text style={styles.buttonText}>{text}</Text>
+        </View>
 
         {loading && (
           <View style={styles.activityIndicatorContainer}>
-            <ActivityIndicator color="white" />
+            <ActivityIndicator color="#8f8d8f" />
           </View>
         )}
       </View>
